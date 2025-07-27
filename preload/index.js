46b6479a12c43ed9cc95db021f0f744ce3225d99
Robert French <178658682+rfrench3@users.noreply.github.com>
2025-07-26 20:10:54 -0400

    remove unused modules, load mouse disappear module

--- preload/index.js
+++ preload/index.js
@@ -1,8 +1,16 @@
-// Load controller support
 const controllerSupport = require('./modules/controller-support')
+const mouseDisappear = require('./modules/mouse-disappear')
+
 try {
     controllerSupport()
     console.log('Controller support loaded globally')
 } catch (err) {
     console.error('Controller support failed to load:', err)
+}
+
+try {
+    mouseDisappear()
+    console.log('Mouse disappear loaded globally')
+} catch (err) {
+    console.error('Mouse disappear failed to load:', err)
 }
--- preload/modules/fix-voice.js
+++
@@ -1,7 +0,0 @@
-//fix voice search
-
-const configOverrides = require('../util/configOverrides')
-
-module.exports = () => {
-    configOverrides.overrideEnv('env_enableMediaStreams', true)
-}
